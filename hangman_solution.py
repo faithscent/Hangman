ecc7cf5bbb171c7203758068ea6eacc993aac6b3
Ruth <r.faith.w@gmail.com>
2022-08-04 20:47:28 +0100

    adds relevant docstring

--- hangman_solution.py
+++ hangman_solution.py
@@ -1,11 +1,41 @@
-'''
-Make sure you complete all the TODOs in this file.
-The prints have to contain the same text as indicated, don't add any more prints,
-or you will get 0 for this assignment.
-'''
 import random
 
 class Hangman:
+
+    '''
+    A Hangman Game that asks the user for a letter and checks if it is in the word.
+    It starts with a default number of lives and a random word from the word_list.
+
+    
+    Parameters:
+    ----------
+    word_list: list
+        List of words to be used in the game
+    num_lives: int
+        Number of lives the player has
+    
+    Attributes:
+    ----------
+    word: str
+        The word to be guessed picked randomly from the word_list
+    word_guessed: list
+        A list of the letters of the word, with '_' for each letter not yet guessed apple
+        For example, if the word is 'apple', the word_guessed list would be ['_', '_', '_', '_', '_']
+        If the player guesses 'p', the list would be ['_', 'p', 'p', '_', '_']
+    num_letters: int
+        The number of UNIQUE letters in the word that have not been guessed yet
+    num_lives: int
+        The number of lives the player has
+    list_letters: list
+        A list of the letters that have already been tried
+
+    Methods:
+    -------
+    check_letter(letter)
+        Checks if the letter is in the word.
+    ask_letter()
+        Asks the user for a letter.
+    '''
     word = ''
     word_guessed =[]
     num_letter = len(set(word))
@@ -25,6 +55,18 @@ class Hangman:
         
 
     def check_letter(self, letter) -> None:
+
+        '''
+        Checks if the letter is in the word.
+        If it is, it replaces the '_' in the word_guessed list with the letter.
+        If it is not, it reduces the number of lives by 1.
+
+        Parameters:
+        ----------
+        letter: str
+            The letter to be checked
+
+        '''
         
         if letter in self.word:
 
@@ -59,6 +101,13 @@ class Hangman:
 
 
     def ask_letter(self):
+
+        '''
+        Asks the user for a letter and checks two things:
+        1. If the letter has already been tried
+        2. If the character is a single character
+        If it passes both checks, it calls the check_letter method.
+        '''
 
         letter = input("Please, enter just one character: \n")
     
